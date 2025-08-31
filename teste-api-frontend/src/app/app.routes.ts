fc01038c606ae194f4192bc7f66b83e9d3bae35d
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2025-08-31 15:34:47 -0300

    finalizado

--- teste-api-frontend/src/app/app.routes.ts
+++ teste-api-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PagamentoListaComponent } from './pagamento-lista-component/pagamento-lista-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: PagamentoListaComponent  // ← APENAS este na raiz
+  },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
--- teste-api-frontend/src/app/app.ts
+++ teste-api-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { PagamentoListaComponent } from "./pagamento-lista-component/pagamento-lista-component";
+import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
-  imports: [  PagamentoListaComponent],
+  imports: [  PagamentoListaComponent, RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
